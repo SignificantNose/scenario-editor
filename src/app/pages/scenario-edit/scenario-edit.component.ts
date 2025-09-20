b21fb72293214f379fc2ecb53c06ae35affb9e33
significantnose <yanucevich.d@gmail.com>
2025-09-20 13:21:20 +0300

    feat(app): example scenarios added

--- src/app/pages/scenario-edit/scenario-edit.component.ts
+++ src/app/pages/scenario-edit/scenario-edit.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ScenarioDesignerComponent } from '@shared/scenario-designer/scenario-designer.component';
+import { ScenarioModel } from '@models/scenario.model';
 
 @Component({
   selector: 'app-scenario-edit',
@@ -11,7 +12,32 @@ import { ScenarioDesignerComponent } from '@shared/scenario-designer/scenario-de
   imports: [CommonModule, ScenarioDesignerComponent],
 })
 export class ScenarioEditComponent {
-  constructor(private router: Router) {}
+  exampleScenario: ScenarioModel = {
+    id: '1',
+    name: 'aslkndf',
+    createdAt: '',
+    emitters: [
+      {
+        id: '1',
+        position: {
+          x: 1,
+          y: 1,
+          z: 1,
+        },
+      },
+    ],
+    listeners: [
+      {
+        id: '1',
+        position: {
+          x: 2,
+          y: 3,
+          z: 1,
+        },
+      },
+    ],
+  };
+  constructor(private router: Router) { }
 
   cancel() {
     this.router.navigate(['/']);
