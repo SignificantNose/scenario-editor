b4829fc2a7b052e63850807f9bb71edc275b633d
significantnose <yanucevich.d@gmail.com>
2025-09-27 18:52:51 +0300

    feat(app): nord color scheme introduced, page colors modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
